1bf8d06930d2a4081bd6e13bad81ccf1dd47056f
Stephen Herbein <stephen272@gmail.com>
2019-03-15 09:31:19 -0700

    all the changes!

--- test/daemon.py
+++ test/daemon.py
@@ -2,17 +2,57 @@
 
 import os
 import time
+from datetime import datetime
 from enum import Enum
 from pathlib import PosixPath
 import argparse
 import subprocess
-from collections import deque, namedtuple
+import random
+from collections import deque, namedtuple, defaultdict
+
+from slackclient import SlackClient
+
+class WeightNotStableError(RuntimeError):
+    pass
 
 def get_current_weight():
     query_cmd = [str(args.query_exe.resolve())]
     if args.query_exe.name == "fake-query.py":
         query_cmd.extend(['-p', str(args.pot), '-w', str(args.water), '-t', str(args.tolerance)])
-    return float(subprocess.check_output(query_cmd))
+    subproc = subprocess.Popen(query_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = subproc.communicate()
+    if stderr and len(stderr) > 0:
+        try:
+            stderr = stderr.decode('utf-8')
+        except UnicodeDecodeError:
+            raise RuntimeError("Failed to decode stderr: {}".format(stderr.hex()))
+        if "Over capacity" in stderr:
+            return float("inf")
+        elif "Under capacity" in stderr:
+            return 0.0
+        elif "Weight not stable" in stderr:
+            raise WeightNotStableError()
+        else:
+            raise RuntimeError()
+    return float(stdout)
+
+def get_notification_template():
+    return random.choice([
+        "{:.1f} ounces of hot, fresh coffee are ready for consumption in B451.",
+        "Please drink the {:.1f} ounces of coffee in B451.  You have 20 seconds to comply.",
+        "You must consume the {:.1f} ounces of coffee or be terminated.",
+        "{:.1f} ounces of human productivity beverage available for consumption in B451",
+        ])
+
+def send_slack_notification(weight_ounces, channel="#flux"):
+    now = datetime.now()
+    if ((now - send_slack_notification.last_notify[channel]).total_seconds() > 60 * 60):
+        message_template = get_notification_template()
+        message = message_template.format(weight_ounces)
+        print("Sending slack notification: {}".format(message))
+        sc.api_call("chat.postMessage", channel=channel, text=message)
+        send_slack_notification.last_notify[channel] = now
+send_slack_notification.last_notify = defaultdict(lambda: datetime.fromtimestamp(0))
 
 class ScaleStates(Enum):
     NOPOT = "no pot"
@@ -56,23 +96,33 @@ class Reading():
 
 def control_loop():
     past_states = deque(maxlen=2)
+    slack_message_already_sent = False
     while True:
-        curr_state = Reading(get_current_weight())
-        past_states.appendleft(curr_state)
-        if all([x == curr_state for x in past_states]):
-            print('{}'.format(curr_state))
-        else:
-            print("Indeterminate state")
+        try:
+            curr_state = Reading(get_current_weight())
+            past_states.appendleft(curr_state)
+            if all([x == curr_state for x in past_states]):
+                print('{}'.format(curr_state))
+                if curr_state.state == ScaleStates.COFFEEPOT:
+                    fl_ounces = pounds_to_fl_ounces(curr_state.weight - args.pot)
+                    if not slack_message_already_sent:
+                        send_slack_notification(fl_ounces)
+                    slack_message_already_sent = True
+                else:
+                    slack_message_already_sent = False
+            else:
+                print("Indeterminate state")
+        except WeightNotStableError:
+            pass
+        except RuntimeError as e:
+            print(e)
         time.sleep(args.interval)
-
-def main():
-    control_loop()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # TODO: also support reading from stdin
     parser.add_argument('query_exe', type=PosixPath, help='path to query executable')
-    parser.add_argument('-p', '--pot', type=float, default=1.6,
+    parser.add_argument('-p', '--pot', type=float, default=1.75,
                         help="weight (in lbs) of the empty coffee pot")
     parser.add_argument('-w', '--water', type=float, default=2.6,
                         help="maximum amount of water (or coffee) that the pot can hold (in lbs)")
@@ -82,4 +132,9 @@ if __name__ == "__main__":
                         help="monitor weight every X seconds")
     args = parser.parse_args()
 
-    main()
+    random.seed()
+
+    slack_token = os.environ["SLACK_API_TOKEN"]
+    sc = SlackClient(slack_token)
+
+    control_loop()
